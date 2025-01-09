b4fa404ed3815bc97f63a1d16f1082da550da8f9
Jaeyeol Song <songjy92@gmail.com>
2025-01-09 23:23:27 +0900

    0109 widget UI

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,86 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-              title: const Text('Hello flutter!',
-                  style: TextStyle(color: Colors.white)),
-              backgroundColor: Colors.blueGrey),
-          body: Center(
-            child: Text('Hello World!'),
-          )),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            backgroundColor: Color(0xFF181818),
+            body: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Hey selean',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                  color: Colors.white54, fontSize: 18),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                    Text('Total Balance',
+                        style: TextStyle(
+                          color: Colors.white54,
+                          fontSize: 25,
+                        )),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text('\$5 194 482',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 42,
+                            fontWeight: FontWeight.w700)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(45)),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 20,
+                              horizontal: 50,
+                            ),
+                            child: Text(
+                              'Transfer',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ))));
   }
 }
